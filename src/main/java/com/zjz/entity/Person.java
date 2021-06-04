1bb2dadbf50d70aa94fc7f6ff7ce52073a606b63
zhangjunzhou <zhangjunzhou@meituan.com>
2021-06-04 15:23:52 +0800

    修改了Person类的get set方法

--- src/main/java/com/zjz/entity/Person.java
+++ src/main/java/com/zjz/entity/Person.java
@@ -4,6 +4,7 @@ import com.sun.istack.internal.NotNull;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -15,6 +16,7 @@ import org.springframework.validation.annotation.Validated;
 @Component
 @ConfigurationProperties(prefix = "person")
 @Validated
+@Data
 public class Person {
     private String name;
     private Integer age;
@@ -36,74 +38,5 @@ public class Person {
         this.maps = maps;
         this.lists = lists;
         this.dog = dog;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Boolean getHappy() {
-        return happy;
-    }
-
-    public void setHappy(Boolean happy) {
-        this.happy = happy;
-    }
-
-    public Date getBirth() {
-        return birth;
-    }
-
-    public void setBirth(Date birth) {
-        this.birth = birth;
-    }
-
-    public Map<String, Object> getMaps() {
-        return maps;
-    }
-
-    public void setMaps(Map<String, Object> maps) {
-        this.maps = maps;
-    }
-
-    public List<Object> getLists() {
-        return lists;
-    }
-
-    public void setLists(List<Object> lists) {
-        this.lists = lists;
-    }
-
-    public Dog getDog() {
-        return dog;
-    }
-
-    public void setDog(Dog dog) {
-        this.dog = dog;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", happy=" + happy +
-                ", birth=" + birth +
-                ", maps=" + maps +
-                ", lists=" + lists +
-                ", dog=" + dog +
-                '}';
     }
 }
